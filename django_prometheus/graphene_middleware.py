d02684fca4d92c7c7e51a51c6e69ca945900dacb
Anton Marini <vade@vade.info>
2023-08-15 15:53:00 -0400

    Tab error

--- django_prometheus/graphene_middleware.py
+++ django_prometheus/graphene_middleware.py
@@ -4,18 +4,18 @@ from django_prometheus import middleware
 """ This will hold arbtrary graphql resolvers by their resolver name while also grabbing existing metrics for Django."""
 class GrapheneMetrics(middleware.Metrics):
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.graphene_resolvers = { }  
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+		self.graphene_resolvers = { }  
 
 
-    def register_graphene_metric(self, metric_cls, name, documentation, labelnames=(), **kwargs):
-    	graphene_metric = metric_cls(name, documentation, labelnames=labelnames, **kwargs)
-    	self.graphene_resolvers[name] = graphene_metric
+	def register_graphene_metric(self, metric_cls, name, documentation, labelnames=(), **kwargs):
+		graphene_metric = metric_cls(name, documentation, labelnames=labelnames, **kwargs)
+		self.graphene_resolvers[name] = graphene_metric
 
 
-    def get_graphene_metric(self, name):
-    	return self.graphene_resolvers[name]
+	def get_graphene_metric(self, name):
+		return self.graphene_resolvers[name]
 
 ''' Overload the metrics class our middleware uses'''
 class GraphenePrometheusBeforeMiddleware(middleware.PrometheusBeforeMiddleware):
@@ -30,16 +30,16 @@ class GraphenePrometheusAfterMiddleware(middleware.PrometheusAfterMiddleware):
 class GrapheneMetricDjangoFilterConnectionField(DjangoFilterConnectionField):
 
 	def __init__(
-	        self,
-	        type_,
-	        fields=None,
-	        order_by=None,
-	        extra_filter_meta=None,
-	        filterset_class=None,
-	        *args,
-	        **kwargs
-	    ):
-	     
+			self,
+			type_,
+			fields=None,
+			order_by=None,
+			extra_filter_meta=None,
+			filterset_class=None,
+			*args,
+			**kwargs
+		):
+		 
 		super().__init__(
 			type_, 
 			fields=fields,
@@ -54,8 +54,9 @@ class GrapheneMetricDjangoFilterConnectionField(DjangoFilterConnectionField):
 
 
 	def resolve_queryset(cls, connection, iterable, info, args, filtering_args, filterset_class):
-    	result = super().resolve_queryset(connection, iterable, info, args, filtering_args, filterset_class)
 
-        GrapheneMetrics.get_instance().get_graphene_metric( str(self.model) ).observe( len(result) )
+		result = super().resolve_queryset(connection, iterable, info, args, filtering_args, filterset_class)
 
-    	return result
+		GrapheneMetrics.get_instance().get_graphene_metric( str(self.model) ).observe( len(result) )
+
+		return result
